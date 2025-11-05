a915e1ed3f20fc09db1aaa4e15684b44e70f7ad0
Pratik <pratikmanjarekar5@gmail.com>
2025-11-05 19:09:07 +0530

    first commit

---
+++ DAA/daa_exp_1.py
@@ -0,0 +1,35 @@
+# Non-recursive Fibonacci Function
+def non_recu_fib(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    a,b = 0,1
+    for i in range(2, n+1):
+        a,b = b, a+b
+    return b
+
+# recursive Fibonacci Function 
+def recu_fib(n):
+    if n <=0:
+        return 0
+    elif n ==1:
+        return 1
+    return recu_fib(n-1) + recu_fib(n-2)
+
+# Time & Space complexity
+def time_space_complexity():
+    print("\nFor Non-recursive Fibonacci :")
+    print("Time Complexity : O(n)")
+    print("Space Complexity : O(1)")
+
+    print("\nFor Recursive Fibonacci :")
+    print("Time Complexity : O(2^n)")
+    print("Space Complexity : O(n)")
+
+# Example
+n = int(input("\nEnter a number :"))
+print(f"Non-recursive Fibonacci of {n} : {non_recu_fib(n)}")
+print(f"Recursive Fibonacci of {n} : {recu_fib(n)}")
+
+time_space_complexity()
